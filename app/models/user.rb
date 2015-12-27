777cfeb753ccf9761be83965a3f1708762509083
Nicole Hall <nicolealexandrahall@gmail.com>
2015-12-27 12:54:45 -0700

    added logic for attaching a subscription to a user. a users subscription defaults to false, but upon user creation, a subscription object is attached.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_one :subscription
+
+  #need to make sure that when a user
+  #is created, a corresponding subscription is created as well
+
+  after_create :create_subscription
+
+  def create_subscription
+    Subscription.create(user_id: id) if subscription.nil?
+  end
 end
